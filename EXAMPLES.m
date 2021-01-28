6e2f6bc755d201481009e1278651ed5fd48a97dd
tamaskis <tamas.a.kis@outlook.com>
2021-01-28 00:55:52 -0800

    Add files via upload

---
+++ EXAMPLES.m
@@ -0,0 +1,60 @@
+%% Copyright (c) 2021 Tamas Kis
+
+% Examples for using the planet3D function.
+
+
+
+%% SCRIPT SETUP
+
+% clears variables and command window, closes all figures
+clear;
+clc;
+close all;
+
+
+
+%% EXAMPLES (UNCOMMENT TO TEST, LIGHT ELEMENT INCLUDED AT VERY END)
+
+% Sun
+%figure;planet3D('Sun',[],[],'Milky Way');
+
+% Moon
+%figure;planet3D('Moon',[],[],'Milky Way');
+
+% Mercury
+%figure;planet3D('Mercury',[],[],'Milky Way');
+
+% Venus
+%figure;planet3D('Venus',[],[],'Milky Way');
+
+% Earth
+%figure;planet3D('Earth',[],[],'Milky Way');
+
+% Earth (Cloudy)
+%figure;planet3D('Earth Cloudy',[],[],'Milky Way');
+
+% Earth (Night)
+%figure;planet3D('Earth Night',[],[],'Milky Way');
+
+% Earth (Night, Cloudy)
+%figure;planet3D('Earth Night Cloudy',[],[],'Milky Way');
+
+% Mars
+%figure;planet3D('Mars',[],[],'Milky Way');
+
+% Jupiter
+%figure;planet3D('Jupiter',[],[],'Milky Way');
+
+% Saturn
+%figure;planet3D('Saturn',[],[],'Milky Way');
+
+% Uranus
+%figure;planet3D('Uranus',[],[],'Milky Way');
+
+% Neptune
+%figure;planet3D('Neptune',[],[],'Milky Way')
+
+% Pluto
+%figure;planet3D('Pluto',[],[],'Milky Way');
+
+light('Position',[1 -1 0]);
